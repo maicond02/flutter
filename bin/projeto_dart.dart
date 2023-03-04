f336340ed30beeeae010bf26db26657e959ef285
maicond02 <eumaicondouglas.md45@gmail.com>
2023-03-04 12:04:24 -0300

    Variaveis Dart

--- bin/projeto_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:projeto_dart/projeto_dart.dart' as projeto_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${projeto_dart.calculate()}!');
-}
---
+++ lib/1_variaveis/a1_variaveis.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  num qualquerNumber = 1;
+  num qualquerNumber2 = 5.2;
+
+  int idade = 21;
+  double valorIphone = 12.000;
+
+  String nome = 'Maicon Alves';
+
+  var number = 21;
+
+  Object objetoQualquer = 0;
+
+  dynamic dynamicQualquer = 'Teste';
+
+  print(dynamicQualquer);
+
+} 
